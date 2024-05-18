a77442b399c8728b786fb7e241d3930514e6c7bd
Kim Gomes <142019936+barbiedeti@users.noreply.github.com>
2024-05-18 17:52:20 -0300

    PT-BR_IA_sentimentos.py
    Versão atualizada pra português

--- IA_sentimentos.py
+++ IA_sentimentos.py
@@ -1,38 +1,39 @@
 import re
-import nltk 
-from nltk.corpus import opinion_lexicon
-nltk.download('opinion_lexicon')
+#import nltk 
+#from nltk.corpus import opinion_lexicon
+#nltk.download('opinion_lexicon')
+
+def carrega_lexico():
+    sentilexpt = open('SentiLex-lem-PT02.txt','r')
+    dic_palavra_polaridade = {}
+    for i in sentilexpt.readlines():
+        pos_ponto = i.find('.')
+        palavra = (i[:pos_ponto])
+        pol_pos = i.find('POL')
+        polaridade = (i[pol_pos+7:pol_pos+9]).replace(';','')
+        dic_palavra_polaridade[palavra] = polaridade
+    return dic_palavra_polaridade
 
 def analyze_sentiment():
-    comentario = input()
+    comentario = input("Como você está se sentindo hoje? ")
 
     # Divisão do comentário em palavras
     palavras = re.findall(r'\b\w+\b', comentario.lower())
 
-    positivas = set(opinion_lexicon.positive())
-    negativas = set(opinion_lexicon.negative())
+    sentilex = carrega_lexico()
 
     # Contagem de palavras positivas, negativas e neutras
-    count_positivo = sum(palavra in positivas for palavra in palavras)
-    count_negativo = sum(palavra in negativas for palavra in palavras)
+    count_positivo = sum(sentilex.get(palavra, 0) == '1' for palavra in palavras)
+    count_negativo = sum(sentilex.get(palavra, 0) == '-1' for palavra in palavras)
+    count_neutro = sum(sentilex.get(palavra, 0) == '0' for palavra in palavras)
 
-    # Verifica se há negação na frase
-    for i, palavra in enumerate(palavras[:-1]):
-        if palavra == "não" and (palavras[i+1] in positivas or palavras[i+1] in negativas):
-            if palavras[i+1] in positivas:
-                count_positivo -= 1
-                count_negativo += 1
-            else:
-                count_negativo -= 1
-                count_positivo += 1
-
-    # Verifica se há mais palavras positivas do que negativas.
+    # Verifica se há mais palavras positivas do que negativas e neutras no comentário. Se essa condição for verdadeira, o comentário é considerado positivo.
     if count_positivo > count_negativo:
         return "Positivo"
     elif count_negativo > count_positivo:
         return "Negativo"
     else:
-        return "Indeterminado"
+        return "Neutro" 
     
 
 # Saída esperada
